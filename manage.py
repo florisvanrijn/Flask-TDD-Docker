be729af1f7676887a74710d6fd05592d5e9a8a22
floris van Rijn <florisvanrijn1@gmail.com>
2020-01-10 23:53:04 +0100

    start of pytest unittests

--- manage.py
+++ manage.py
@@ -1,11 +1,17 @@
 # manage.py
 
 from flask.cli import FlaskGroup
-from project import app
+from project import app, db
 
 # creating a new FlaskGroup instance to extend the normal CLI with commands related to the Flask App.
 cli = FlaskGroup(app)
 
+@cli.command('recreate_db')
+def recreate_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+    
 if __name__ == '__main__':
     cli()
 
