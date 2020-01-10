7c0f91a5b8e37f9921c5570ef424c14fd22e3165
floris van Rijn <florisvanrijn1@gmail.com>
2020-01-10 23:30:27 +0100

    postgres database functionality

--- project/config.py
+++ project/config.py
@@ -1,17 +1,22 @@
 #Defining environment-specific configuration variables
 # project/config.py
+import os  # new
+
 
 class BaseConfig:
     TESTING = False
-    SQLAlchemy_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 class DevelopmentConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 class TestingConfig(BaseConfig):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
 
+
 class ProductionConfig(BaseConfig):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    
+
