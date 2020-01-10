bad5685cab19cbbb72cc0d1e66e6a6eabcfd85d7
floris van Rijn <florisvanrijn1@gmail.com>
2020-01-10 18:11:52 +0100

    postgress database addition

--- project/__init__.py
+++ project/__init__.py
@@ -1,10 +1,9 @@
 # project/__init__.py
 
-import sys
 import os  # new
 from flask import Flask, jsonify
 from flask_restplus import Resource, Api
-
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -12,10 +11,25 @@ app = Flask(__name__)
 api = Api(app)
 
 # set config
-app_settings = os.getenv('APP_SETTINGS')  # new
-app.config.from_object(app_settings)      # new
+app_settings = os.getenv('APP_SETTINGS')  
+app.config.from_object(app_settings)      
 
-print(app.config, file=sys.stderr)
+# instantiate the database
+db = SQLAlchemy(app)
+
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
 class Ping(Resource):
     def get(self):
         return {
