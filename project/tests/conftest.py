5acdf1fce9997d261dab1a0df4b63305e154edcb
floris van Rijn <florisvanrijn1@gmail.com>
2020-01-13 00:05:14 +0100

    setup working with app and db context directly

--- project/tests/conftest.py
+++ project/tests/conftest.py
@@ -3,19 +3,21 @@
 #Fixtures are reusable objects for tests. They have a scope which indicates how often the fixture is invoked. 
 import pytest
 
-from project import app, db
+from project import create_app, db
+
 
 @pytest.fixture(scope='module')
 def test_app():
+    app = create_app()
     app.config.from_object('project.config.TestingConfig')
     with app.app_context():
-        yield app # testing takes place here
+        yield app  # testing happens here
 
 
 @pytest.fixture(scope='module')
 def test_database():
     db.create_all()
-    yield db # testing takes place here
+    yield db  # testing happens here
     db.session.remove()
     db.drop_all()
 
